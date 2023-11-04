8f02f86c72a5c21ee34178683ac2ac866dc92352
Shreyash2098 <143548443+Shreyash2098@users.noreply.github.com>
2023-11-05 00:02:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,19 @@
-//your code here
-// An array of band names
-let bandNames = ['The Rolling Stones', 'Led Zeppelin', 'Pink Floyd', 'The Beatles', 'The Who'];
+function rot13(str) {
+  return str.replace(/[A-Z]/g, (char) => {
+    // Check if the character is an uppercase letter
+    if (/[A-Z]/.test(char)) {
+      // Decode the character by shifting it back by 13 places
+      const decodedChar = String.fromCharCode(((char.charCodeAt(0) - 'A'.charCodeAt(0) + 13) % 26) + 'A'.charCodeAt(0));
+      return decodedChar;
+    } else {
+      // If the character is not an uppercase letter, return it unchanged
+      return char;
+    }
+  });
+}
 
-// Remove articles from the band names and store them in a new array
-let modifiedBandNames = bandNames.map(name => {
-  // Split the name into words
-  let words = name.split(' ');
-
-  // Remove articles ('a', 'an', 'the') if they are at the beginning of the name
-  while (words.length > 1 && ['a', 'an', 'the'].includes(words[0].toLowerCase())) {
-    words.shift();
-  }
-
-  // Join the words back to form the modified name
-  return words.join(' ');
-});
-
-// Sort the modified band names lexicographically
-modifiedBandNames.sort();
-
-// Create an HTML list
-let ul = document.createElement('ul');
-ul.id = 'band';
-
-// Add list items to the ul
-modifiedBandNames.forEach(name => {
-  let li = document.createElement('li');
-  li.textContent = name;
-  ul.appendChild(li);
-});
-
-// Append the ul to the body or any other element where you want to display it
-document.body.appendChild(ul);
+// Example usage:
+const encodedString = 'SERR PBQR PNZC';
+const decodedString = rot13(encodedString);
+console.log(decodedString); // Output: 'FREE CODE CAMP'
 
